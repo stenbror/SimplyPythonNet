d458c3bb9df7c31d4674eed47a99fd45597acbc5
Richard Magnor Stenbro <stenbror@hotmail.comn>
2025-12-28 00:12:33 +0100

    Started on utillity functions in tokenizer

--- SimplyPythonNet/parser.fs
+++ SimplyPythonNet/parser.fs
@@ -3,5 +3,3 @@
 type AST =
     | Empty
     
-
-
--- SimplyPythonNet/tokenizer.fs
+++ SimplyPythonNet/tokenizer.fs
@@ -3,3 +3,23 @@
 type Token =
     | Empty
     
+
+let OneCharToken c =
+    match c with
+    |   '!' -> Some()
+    |   _ -> None
+    
+let TwoCharToken (c1, c2) =
+     match c1 c2 with
+     | '!', '=' -> Some()
+     |   _ -> None
+     
+let ThreeCharToken (c1, c2, c3)=
+     match c1 c2 c3 with
+     | '<', '<', '=' -> Some()
+     |   _ -> None
+     
+let ReservedKeyword word =
+    match word with
+    |   "False" -> Some()
+    | _ -> None
