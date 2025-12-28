f39a427a323565c62e09362bedff706b330a5fee
Richard Magnor Stenbro <stenbror@hotmail.comn>
2025-12-28 01:00:07 +0100

    Reserved keyword including break

--- SimplyPythonNet/tokenizer.fs
+++ SimplyPythonNet/tokenizer.fs
@@ -1,25 +1,76 @@
 ﻿module SimplyPythonNet.tokenizer
 
 type Token =
-    | Empty
+    |   Empty
+    (* Reserved keywords *)
+    |   False of uint * uint
+    |   None of uint * uint
+    |   True of uint * uint
+    |   And of uint * uint
+    |   As of uint * uint
+    |   Assert of uint * uint
+    |   Async of uint * uint
+    |   Await of uint * uint
+    |   Break of uint * uint
+    |   Class of uint * uint
+    |   Continue of uint * uint
+    |   Def of uint * uint
+    |   Del of uint * uint
+    |   Elif of uint * uint
+    |   Else of uint * uint
+    |   Except of uint * uint
+    |   Finally of uint * uint
+    |   For of uint * uint
+    |   From of uint * uint
+    |   Global of uint * uint
+    |   If of uint * uint
+    |   In of uint * uint
+    |   Is of uint * uint
+    |   Lambda of uint * uint
+    |   Nonlocal of uint * uint
+    |   Not of uint * uint
+    |   Or of uint * uint
+    |   Pass of uint * uint
+    |   Raise of uint * uint
+    |   Return of uint * uint
+    |   Try of uint * uint
+    |   While of uint * uint
+    |   With of uint * uint
+    |   Yield of uint * uint
+    (* Soft keywords *)
+    |   Match of uint * uint
+    |   Case of uint * uint
+    |   Type of uint * uint
+    |   Default of uint * uint (* '_' *)
+    
+    
+
     
 
 let OneCharToken c =
     match c with
-    |   '!' -> Some()
-    |   _ -> None
+    |   '!' -> Some(Token.Empty)
+    |   _ -> Option.None
     
-let TwoCharToken (c1, c2) =
+let TwoCharToken c1 c2 =
      match c1 c2 with
-     | '!', '=' -> Some()
-     |   _ -> None
+     | '!', '=' -> Some(Token.Empty)
+     |   _ -> Option.None
      
-let ThreeCharToken (c1, c2, c3)=
+let ThreeCharToken c1 c2 c3=
      match c1 c2 c3 with
-     | '<', '<', '=' -> Some()
-     |   _ -> None
+     | '<', '<', '=' -> Some(Token.Empty)
+     |   _ -> Option.None
      
 let ReservedKeyword word =
     match word with
-    |   "False" -> Some()
-    | _ -> None
+    |   "False" -> Some(Token.False)
+    |   "None" -> Some(Token.None)
+    |   "True" -> Some(Token.True)
+    |   "and" -> Some(Token.And)
+    |   "as" -> Some(Token.As)
+    |   "assert" -> Some(Token.Assert)
+    |   "async" -> Some(Token.Async)
+    |   "await" -> Some(Token.Await)
+    |   "break" -> Some(Token.Break)
+    |   _ -> Option.None
