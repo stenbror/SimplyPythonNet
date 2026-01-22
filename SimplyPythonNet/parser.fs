53106f425218015d22c82ec1acc2541f75f97ac2
Richard Magnor Stenbro <stenbror@hotmail.com>
2026-01-22 20:55:58 +0100

    First test for atom rule

--- SimplyPythonNet/parser.fs
+++ SimplyPythonNet/parser.fs
@@ -12,19 +12,24 @@ type AST =
     |   Name of uint * uint * string
     |   String of uint * uint * string list
     
-type NodeThree = Result<(AST * TokenStream), string>
+type NodeThree = Result<(AST * TokenStream), (string * uint)>
     
 
 (* Expression rules *)
-let rec ParseAtom (stream: TokenStream ) : NodeThree =
+let rec ParseAtom (stream: Result<TokenStream, (string * uint)> ) : NodeThree =
     match stream with
-    | [] -> Error "Unexpected end of input"
-    | first :: rest ->
-        match first with
-        |   Token.True(s, e) -> Ok(AST.True(s, e), rest)
-        |   Token.False(s, e) -> Ok(AST.False(s, e), rest)
-        |   Token.None(s, e) -> Ok(AST.None(s, e), rest)
-        |   Token.Ellipsis(s, e) -> Ok(AST.Ellipsis(s, e), rest)
-        |   Token.NameLiteral(s, e, t) -> Ok(AST.Name(s, e, t), rest)
-        |   Token.NumberLiteral(s, e, t) -> Ok(AST.Number(s, e, t), rest)
-        |   _ -> Error "Unexpected token"
+    |   Error(e, p) -> Error(e, p)
+    |   Ok(stream_ok) ->
+    
+    
+        match stream_ok with
+        | [] -> Error ("Unexpected end of input", 0u)
+        | first :: rest ->
+            match first with
+            |   Token.True(s, e) -> Ok(AST.True(s, e), rest)
+            |   Token.False(s, e) -> Ok(AST.False(s, e), rest)
+            |   Token.None(s, e) -> Ok(AST.None(s, e), rest)
+            |   Token.Ellipsis(s, e) -> Ok(AST.Ellipsis(s, e), rest)
+            |   Token.NameLiteral(s, e, t) -> Ok(AST.Name(s, e, t), rest)
+            |   Token.NumberLiteral(s, e, t) -> Ok(AST.Number(s, e, t), rest)
+            |   _ -> Error ("Unexpected token", 0u)
---
+++ TestSimplyPythonNet/TestParser.fs
@@ -0,0 +1,25 @@
+﻿module TestSimplyPythonNet.TestParser
+
+open Xunit
+open SimplyPythonNet.tokenizer
+open SimplyPythonNet.parser
+
+[<Fact>]
+let ``Atom rule: None`` () =
+    let result = "None" |> Tokenize |> ParseAtom
+    Assert.Equal(Ok(AST.None(0u, 4u), []), result)
+
+[<Fact>]
+let ``Atom rule: True`` () =
+    let result = "True" |> Tokenize |> ParseAtom
+    Assert.Equal(Ok(AST.True(0u, 4u), []), result)
+    
+[<Fact>]
+let ``Atom rule: False`` () =
+    let result = "False" |> Tokenize |> ParseAtom
+    Assert.Equal(Ok(AST.False(0u, 5u), []), result)
+    
+[<Fact>]
+let ``Atom rule: ...`` () =
+    let result = "..." |> Tokenize |> ParseAtom
+    Assert.Equal(Ok(AST.Ellipsis(0u, 3u), []), result) 
