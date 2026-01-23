4c572e04a03da8d37b28f4cdede95a0b25b8eecc
Richard Magnor Stenbro <stenbror@hotmail.com>
2026-01-23 19:31:31 +0100

    Added rule for ParseAwaitPrimary

--- SimplyPythonNet/parser.fs
+++ SimplyPythonNet/parser.fs
@@ -11,8 +11,15 @@ type AST =
     |   Number of uint * uint * string
     |   Name of uint * uint * string
     |   String of uint * uint * string list
+    |   Await of uint * uint * AST
     
 type NodeThree = Result<(AST * TokenStream), (string * uint)>
+
+(* Utilities functions*)
+let GetStartOfTokenInStream (tokens: TokenStream) : uint =
+    match tokens with
+    |   [] -> 0u
+    |   first :: rest -> GetTokenStartPosition first
     
 (* Expression rules *)
 let rec ParseAtom (stream: Result<TokenStream, (string * uint)> ) : NodeThree =
@@ -47,4 +54,48 @@ let rec ParseAtom (stream: Result<TokenStream, (string * uint)> ) : NodeThree =
                             |   _ -> false
                         do ()
                     Ok(AST.String(start, _end, List.rev text), rest_symbols)
+            |   _  -> Error ("Unexpected token", GetTokenStartPosition first)
+            
+and ParsePrimary (stream: Result<TokenStream, (string * uint)> ) : NodeThree =
+    match stream with
+    |   Error(e, p) -> Error(e, p)
+    |   Ok(stream_ok) ->
+        match stream_ok with
+        | [] -> Error ("Unexpected end of input", 0u)
+        | first :: rest ->
+            match first with
+            |   _  -> Error ("Unexpected token", GetTokenStartPosition first)
+
+and ParseAwaitPrimary (stream: Result<TokenStream, (string * uint)> ) : NodeThree =
+    match stream with
+    |   Error(e, p) -> Error(e, p)
+    |   Ok(stream_ok) ->
+        match stream_ok with
+        | [] -> Error ("Unexpected end of input", 0u)
+        | first :: rest ->
+            match first with
+            |   Token.Await(s, e) ->
+                    let right = ParsePrimary (Ok(rest))
+                    match right with
+                    |   Ok(right_ok) ->
+                            let right3, rest2 = right_ok
+                            Ok(AST.Await(s, GetStartOfTokenInStream rest2, right3), rest2)
+                    |   Error(e, p) -> Error(e, p)
+                    
+            |   _  -> Error ("Unexpected token", GetTokenStartPosition first)
+            
+            
+            
+            
+            
+
+// Empty template function to be used for rules ///////////////////////////////////////////////////////////////////////        
+let DummyFunction (stream: Result<TokenStream, (string * uint)> ) : NodeThree =
+    match stream with
+    |   Error(e, p) -> Error(e, p)
+    |   Ok(stream_ok) ->
+        match stream_ok with
+        | [] -> Error ("Unexpected end of input", 0u)
+        | first :: rest ->
+            match first with
             |   _  -> Error ("Unexpected token", GetTokenStartPosition first)
